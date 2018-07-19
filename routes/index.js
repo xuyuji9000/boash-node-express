d86d0301b228942196315ee0fa5d167578d77253
Karl Xu <karl.yogi.xu@gmail.com>
2018-07-19 11:36:27 +0800

    new commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express V2' });
+  res.render('index', { title: 'Express V3' });
 });
 
 module.exports = router;
